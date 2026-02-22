4113bc1d1c8d62be2835000ab176fffa521d1fdd
Thomas Nah <nahthomas0@gmail.com>
2026-02-22 16:02:06 +0100

    some changes

--- app.vue
+++ app.vue
@@ -28,7 +28,7 @@
             >Join Us</NuxtLink
           >
           <NuxtLink
-            to="/how-we-work"
+            to="/work"
             class="px-4 py-2 rounded-full text-sm font-medium text-brand-black transition-all"
             active-class=""
             >How we work</NuxtLink
@@ -89,7 +89,7 @@
               Join Us
             </NuxtLink>
             <NuxtLink
-              to="/how-we-work"
+              to="/work"
               class="hero-bold text-brand-black font-bold text-5xl"
               @click="isMobileMenuOpen = false"
             >
---
+++ pages/work.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>sSD</h1>
+</template>
